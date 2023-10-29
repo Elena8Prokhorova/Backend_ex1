2c197324af410938ea80781c10ed1b86ee6c3b84
Elena8Prokhorova <144575984+Elena8Prokhorova@users.noreply.github.com>
2023-10-29 20:16:33 +1000

    Шаблон

---
+++ template/src/main/java/com/thewhite/study/Main.java
@@ -0,0 +1,8 @@
+package com.thewhite.study;
+
+public class Main {
+    public static void main(String[] args) {
+        var echo = new Echo();
+        System.out.println(echo.echo("Hello world!"));
+    }
+}
